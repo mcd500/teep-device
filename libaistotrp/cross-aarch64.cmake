213449ec45a7d5c6c9a0d5733e4d2117d9094251
Andy Green <andy@warmcat.com>
2019-01-26 07:51:41 +0800

    make: correct mbedtls path

--- libaistotrp/cross-aarch64.cmake
+++ libaistotrp/cross-aarch64.cmake
@@ -15,7 +15,7 @@ set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
 
 #-nostdlib
-SET(CMAKE_C_FLAGS "-DGCC_VER=\"\\\"$(GCC_VER)\\\"\" -DARM64=1 -D__LP64__=1 -Os -g3 -fpie -mstrict-align -fPIC -ffunction-sections -fdata-sections -L../../mbedtls/build/library" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-DGCC_VER=\"\\\"$(GCC_VER)\\\"\" -DARM64=1 -D__LP64__=1 -Os -g3 -fpie -mstrict-align -fPIC -ffunction-sections -fdata-sections -L../../mbedtls-build/library" CACHE STRING "" FORCE)
 
 
 # Where to look for the target environment. (More paths can be added here)
