9553029f3a6b544715eef5addb26d893e4cb1cea
Yasuaki MORITA <yasuaki.morita@lepidum.co.jp>
2020-01-27 20:03:33 +0900

    fix SEGV

--- pctest/tee_client_api_stub.c
+++ pctest/tee_client_api_stub.c
@@ -4,17 +4,17 @@
 #define EMSG(...) fprintf(stderr, __VA_ARGS__)
 
 /* the TAM root cert we trust */
-static const uint8_t * const tam_root_cert_pem =
+static const char * const tam_root_cert_pem =
 #include "tam_root_cert.h"
 ;
 /* the TAM server cert public key as a JWK */
 
-static const uint8_t * const tam_id_pubkey_jwk =
+static const char * const tam_id_pubkey_jwk =
 #include "tam_id_pubkey_jwk.h"
 ;
 
 /* our TEE private key as a JWK */
-static const uint8_t * const tee_privkey_jwk =
+static const char * const tee_privkey_jwk =
 #include "tee_privkey_jwk.h"
 ;
 
@@ -46,32 +46,30 @@ void TEEC_CloseSession(TEEC_Session *session)
 
 }
 
-static struct lws_context *context;
-
-#define JW_TEMP_SIZE (800 * 1024)
+#define JW_TEMP_SIZE (1024 * 1024 * 10)
+static char temp_buf[JW_TEMP_SIZE];
 
 static TEEC_Result
-otrp(const uint8_t *msg, int msg_len, uint8_t *resp, int resp_len) {
-	struct lws_genhash_ctx hash_ctx;
+otrp(const char *msg, int msg_len, uint8_t *resp, int resp_len) {
+	struct lws_context_creation_info info;
+	static struct lws_context *context;
 	struct lws_jwk jwk_pubkey_tam;
-	int temp_len = JW_TEMP_SIZE;
+	int temp_len = sizeof(temp_buf);
 	TEEC_Result res = 1;
 	struct lws_jws jws;
 	struct lws_jwe jwe;
-	char *temp;
 	int n;
 
 	lws_set_log_level(LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE, NULL);
 
-	temp = malloc(JW_TEMP_SIZE);
-	if (!temp) {
-		EMSG("%s: can't allocate JWS temp\n", __func__);
-		return TEEC_ERROR_BAD_PARAMETERS;
-	}
-
 	EMSG("%s: msg len %d\n", __func__, msg_len);
-
+	EMSG("%d\n", temp_len); 
+	memset(&info, 0, sizeof(info));
+	info.port = CONTEXT_PORT_NO_LISTEN;
+	info.options = 0;
+	context = lws_create_context(&info);
 	lws_jws_init(&jws, &jwk_pubkey_tam, context);
+	lws_jwe_init(&jwe, context);
 
 	if (lws_jwk_import(&jwk_pubkey_tam, NULL, NULL, tam_id_pubkey_jwk,
 				strlen(tam_id_pubkey_jwk))) {
@@ -79,44 +77,26 @@ otrp(const uint8_t *msg, int msg_len, uint8_t *resp, int resp_len) {
 
 		goto bail;
 	}
-
+	EMSG("%d\n", temp_len); 
 	if (lws_jws_sig_confirm_json(msg, msg_len, &jws, &jwk_pubkey_tam, context,
-				temp, &temp_len) < 0) {
+				temp_buf, &temp_len) < 0) {
 		lwsl_notice("%s: confirm rsa sig failed\n",
 				__func__);
 		goto bail;
 	}
 
+	EMSG("%d\n", temp_len); 
+
 	lwsl_user("Signature OK\n");
 
-	/*
-	 * it's a bit expensive, but take a moment to reset the temp usage now
-	 * the JWS part is confirmed.  Over-allocating large temp spaces is
-	 * really expensive in OP-TEE world.
-	 *
-	 * First copy the part we care about, the base64-decoded JWS payload,
-	 * to the start of the temp space.
-	 */
-
-	memmove(temp, jws.map.buf[LJWS_PYLD], jws.map.len[LJWS_PYLD]);
-
-	/* Second, set the pointers to point to the copy at the start */
-
-	jws.map.buf[LJWS_PYLD] = temp;
-
-	/* Lastly, reset the remaining temp space.  Now all the JWS-related
-	 * temp usage is wiped out except the JWE payload. */
-
-	temp_len = JW_TEMP_SIZE - jws.map.len[LJWS_PYLD];
-
-	lws_jwe_init(&jwe, context);
-	if (lws_jwe_json_parse(&jwe, jws.map.buf[LJWS_PYLD],
+	if (lws_jwe_json_parse(&jwe, (uint8_t *)jws.map.buf[LJWS_PYLD],
 				jws.map.len[LJWS_PYLD],
-				lws_concat_temp(temp, temp_len), &temp_len)) {
+				lws_concat_temp(temp_buf, temp_len), &temp_len)) {
 		lwsl_err("%s: lws_jwe_json_parse failed\n",
 				__func__);
 		goto bail1;
 	}
+	EMSG("%d\n", temp_len); 
 
 	if (lws_jwk_import(&jwe.jwk, NULL, NULL, tee_privkey_jwk,
 				strlen(tee_privkey_jwk))) {
@@ -125,15 +105,15 @@ otrp(const uint8_t *msg, int msg_len, uint8_t *resp, int resp_len) {
 		goto bail1;
 	}
 
+	EMSG("%d\n", temp_len); 
 	/* JWS payload is a JWE */
-
-	n = lws_jwe_auth_and_decrypt(&jwe, lws_concat_temp(temp, temp_len),
-			&temp_len);
+	n = lws_jwe_auth_and_decrypt(&jwe, lws_concat_temp(temp_buf, temp_len), &temp_len);
 	if (n < 0) {
 		lwsl_err("%s: lws_jwe_auth_and_decrypt failed\n",
 				__func__);
 		goto bail1;
 	}
+	EMSG("%d\n", temp_len); 
 
 	lwsl_user("Decrypt OK: length %d\n", n);
 
@@ -153,8 +133,6 @@ bail1:
 bail:
 	lws_jwk_destroy(&jwk_pubkey_tam);
 	lws_jws_destroy(&jws);
-
-	free(temp);
 
 	return res;
 }
