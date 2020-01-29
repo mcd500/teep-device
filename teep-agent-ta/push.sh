79171ca6813fbbf2e3ff0e2ff55c497f09c408e0
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-01-29 10:27:45 +0900

    More utilities for push

---
+++ teep-agent-ta/push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git push || true
+cd ..
+git add libteep
+git commit -m "bump submodule"
+git push || true
+cd ..
+git add aist-teep
+git commit -m "bump submodule"
+git push || true
+cd ..
+git add aist-otrp
+git commit -m "bump submodule"
+git push || true
