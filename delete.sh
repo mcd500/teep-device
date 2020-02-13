223a2a88998c04cc5966a152e9a9ad7c9882dce1
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-02-13 09:28:05 +0900

    Were in wrong directory

--- delete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xi
-
-IP=127.0.0.1
-PORT=3000
-
-./teep-broker-app --tamurl http://${IP}:${PORT}/api/tam --talist 8d82573a-926d-4754-9353-32dc29997f74
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xi
-
-IP=127.0.0.1
-PORT=3000
-
-./teep-broker-app --tamurl http://${IP}:${PORT}/api/tam
