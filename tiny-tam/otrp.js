72d14513b8ec8928e1db806e43861cd1f2899221
Yasuaki MORITA <yasuaki.morita@lepidum.co.jp>
2020-02-14 14:53:53 +0900

    add tiny tam otrp message handling

--- tiny-tam/otrp.js
+++ tiny-tam/otrp.js
@@ -1,92 +1,164 @@
 const {JWK, JWS, JWE} = require('node-jose');
-module.exports = (tamPrivKey, teePubKey, taImage) => ({
-	token: 0,
+
+const VER = "1.0"
+const TAMID = "id1.TAMxyz.com"
+const TA_UUID = "8d82573a-926d-4754-9353-32dc29997f74"
+const OCSPDAT = "sample ocspdat B64 encoded ASN1"
+const signParam = {
+	alg: 'RS256',
+	format: 'flattened'
+}
+const encParam = {
+	fields: {
+		alg: 'RSA1_5'
+	},
+	contentAlg: "A128CBC-HS256",
+	format: 'flattened'
+}
+module.exports = (tamPrivKey, teePubKey, taImage, taUrl) => ({
+	rid: 0,
 	session: {},
-	sign(data) {return JWS.createSign({alg: 'RS256', format: 'flattend'}, tamPrivKey).update(data).final()},
+	sign(data) {return JWS.createSign(signParam, tamPrivKey).update(data).final()},
 	verify(data) {return JWS.createVerify(teePubKey).verify(data)},
-	encrypt(data) {return JWE.createEncrypt({ alg: 'RSA1_5', contentAlg: 'A128CBC-HS256', format: 'flattened' }, teePubKey).update(data).final()},
-	decrypt(data) {return JWE.crateDecrypt(tamPrivKey).decrypt(data)},
+	encrypt(data) {return JWE.createEncrypt(encParam, teePubKey).update(data).final()},
+	decrypt(data) {return JWE.createDecrypt(tamPrivKey).decrypt(data)},
 
 	async wrap(data) {
+		console.log(data)
 		signed = await this.sign(JSON.stringify(data))
-		encrypted = await this.encrypt(JSON.stringify(signed))
-		return JSON.stringify(encrypted)
+		console.log(signed)
+		return JSON.stringify(signed)
 	},
 
 	async unwrap(data) {
-		decrypted = await this.decrypt(JSON.parse(data))
-		verified = await this.verify(JSON.parse(decrypted.payload))
+		console.log(data)
+		verified = await this.verify(JSON.parse(data))
 		if (verified) {
 			return JSON.parse(verified.payload)
 		}
 		return null
 	},
+
+	async finishTeep(res) {
+		res.statusCode = 204
+		res.setHeader('Content-Length', 0) // if we don't add this header, client wait for rest data
+		res.end()
+	},
+
+	async sendOTrPMessage(jose, mesg, res, statusCode) {
+		this.rid += 1;
+		mesg.rid = this.rid.toString()
+		this.session[mesg.rid] = mesg
+		let wrapped
+		if (jose) {
+			wrapped = await this.wrap(mesg);
+		} else {
+			wrapped = JSON.stringify(mesg)
+		}
+		res.statusCode = statusCode
+		res.setHeader('Content-Type', 'application/teep+json')
+		res.setHeader('Content-Length', wrapped.length)
+		res.end(wrapped)
+	},
+	sendGetDeviceStateRequest(jose, res) {
+		console.log("sendGetDeviceStateRequest");
+		const mes = {
+			GetDeviceStateTBSRequest: {
+				ver: VER,
+				tid: "1",
+				ocspdat: [Buffer.from(OCSPDAT).toString('base64')]
+			}
+		}
+		this.sendOTrPMessage(jose, mes, res, 200)
+	},
+	sendInstallTARequest(jose, res) {
+		console.log("sendInstallTARequest");
+		const mes = {
+			InstallTATBSRequest: {
+				ver: VER,
+				tid: "1",
+				tee: "aist-otrp",
+				nextdsi: false,
+				dsihash: undefined,
+				content: {
+					tamid: TAMID,
+					taid: TA_UUID
+				},
+				encrypted_ta: taImage
+			}
+		}
+		this.sendOTrPMessage(jose, mes, res, 200)
+	},
+	sendDeleteTARequest(jose, res) {
+		console.log("sendDeleteTARequest");
+		const mes = {
+			DeleteTATBSRequest: {
+				ver: VER,
+				rid: "1",
+				tid: "1",
+				tee: "aist-otrp",
+				nextdsi: false,
+				dsihash: undefined,
+				content: {
+					tamid: TAMID,
+					taid: TA_UUID
+				}
+			}
+		}
+		this.sendOTrPMessage(jose, mes, res, 200)
+	},
+
 	async handleMessage(req, body, res) {
-		if (req.url == "/delete") {
-			return this.handleAppDelete(req, null, res);
+		console.log("teep message detected")
+		let jose = false
+		let del = false
+		if (req.url == '/') {
+		} else if (req.url == '/api/tam') {
+		} else if (req.url == '/api/tam_delete') {
+			del = true
+		} else if (req.url == '/api/tam_jose') {
+			jose = true
+		} else if (req.url == '/api/tam_jose_delete') {
+			del = true
+			jose = true
+		} else {
+			console.log("invalid url")
+			res.statusCode = 404
+			res.end()
+			return
 		}
 		if (!body) {
 			// if body is empty, goto OTrP:GetDeviceState or TEEP:QueryRequest
-			return this.handleAppInstall(req, null, res)
-				// handleQuery(req, res);
+			console.log("handle empty message")
+			return this.sendGetDeviceStateRequest(jose, res);
+		}
+		// parse json for switch TEEP Response	
+		let teepRes
+		if (jose) {
+			teepRes = await this.unwrap(body)
 		} else {
-			// parse json for switch TEEP Response
-			let bodyJson = JSON.parse(body);
-			console.log("parsed JSON:", bodyJson);
-
-			if (bodyJson.GetDeviceStateResponse !== undefined) {
-				console.log("reviced GetDeviceStateResponse");
-				// TODO: switch handleAppInstall or handleAppDelete
-				return this.handleAppInstall(req, bodyJson, res);
-			} else if (bodyJson.InstallTAResponse !== undefined) {
-				console.log("reviced InstallTAResponse");
-				// no more sequence, return 204
-				res.statusCode = 204;
-				res.end();
-				return
-			} else if (bodyJson.DeleteTAResponse !== undefined) {
-				console.log("reviced DeleteTAResponse");
-				// no more sequence, return 204
-				res.statusCode = 204;
-				res.end();
-				return
+			teepRes = JSON.parse(body)
+		}
+		if (!teepRes) {
+			console.log("failed to parse json body" , teepRes)
+			return this.finishTeep()
+		}
+		console.log("parsed JSON:", teepRes)
+		if (teepRes.GetDeviceTEEStateTBSResponse) {
+			console.log("detect getDeviceTEEStateTBResonse")
+			if (del) {
+				return this.sendDeleteTARequest(jose, res)
 			} else {
-				// unknown OTrP or TEEP message
-				res.statusCode = 400;
-				console.error("Unknown OTrP or TEEP message");
-				res.end();
-				return
+				return this.sendInstallTARequest(jose, res);
 			}
 		}
-	},
-	// OTrP::InstallTA or TEEP:TrustedAppInstall
-	async handleAppInstall(req, bodyJson, res) {
-		console.log(req.url + ": Request for encrypted TA\n");
-		let message = await this.encrypt(taImage)
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'application/tee-ta');
-		res.setHeader('Content-Length', message.length);
-		res.end(f);
-	},
-
-	// OTrP::DeleteTA or TEEP:TrustedAppDelete
-	async handleAppDelete(req, bodyJson, res) {
-		console.log("Request for delete packet\n");
-		const cmd = "{\"delete-ta\":\"8d82573a-926d-4754-9353-32dc29997f74.ta\"}";
-		let message = await this.wrap(cmd)
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'application/tee-ta');
-		res.setHeader('Content-Length', message.length);
-		res.end(message)
-	},
-
-	// OTrP:GetDeviceState or TEEP:QueryRequest
-	async handleQuery(req, res) {
-		console.log("Request for send QueryRequest\n");
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'application/tee-ta');
-		res.end();
+		if (teepRes.InstallTATBSResponse) {
+			console.log("trusted app install succeed")
+			return this.finishTeep(res)
+		}
+		if (teepReq.DeleteTATBSResponse) {
+			console.log("trusted app delete succeed")
+			return this.finishTeep(res)
+		}
 	}
-
 })
-
