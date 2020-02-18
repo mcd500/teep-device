a1b5c36f9fafdc20748b5076f38351536e5fc185
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-02-18 14:07:37 +0100

    Coding style

--- teep-agent-ta/teep-agent-ta.c
+++ teep-agent-ta/teep-agent-ta.c
@@ -163,39 +163,46 @@ TA_InvokeCommandEntryPoint(void __maybe_unused *sess_ctx,
 	switch (cmd_id) {
 	case TEEP_AGENT_TA_WRAP_MESSAGE: /* wrap TEEP message */
 		if ((TEE_PARAM_TYPE_GET(param_types, 0) != TEE_PARAM_TYPE_MEMREF_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_MEMREF_OUTPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_VALUE_INOUT))
+		    (TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_MEMREF_OUTPUT) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_VALUE_INOUT))
 			return TEE_ERROR_BAD_PARAMETERS;
-		return teep_message_wrap(params[0].memref.buffer, params[1].value.a, params[2].memref.buffer, &params[3].value.a);
+		return teep_message_wrap(params[0].memref.buffer,
+					 params[1].value.a, params[2].memref.buffer,
+					 &params[3].value.a);
 	case TEEP_AGENT_TA_UNWRAP_MESSAGE: /* unwrap TEEP message */
 		if ((TEE_PARAM_TYPE_GET(param_types, 0) != TEE_PARAM_TYPE_MEMREF_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_MEMREF_OUTPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_VALUE_INOUT))
+		    (TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_MEMREF_OUTPUT) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_VALUE_INOUT))
 			return TEE_ERROR_BAD_PARAMETERS;
-		return teep_message_unwrap(params[0].memref.buffer, params[1].value.a, params[2].memref.buffer, &params[3].value.a);
+		return teep_message_unwrap(params[0].memref.buffer,
+					   params[1].value.a, params[2].memref.buffer,
+					   &params[3].value.a);
 	case TEEP_AGENT_TA_INSTALL: /* Install TA */
 		if ((TEE_PARAM_TYPE_GET(param_types, 0) != TEE_PARAM_TYPE_MEMREF_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_NONE) ||
-				(TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_NONE))
+		    (TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_NONE) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_NONE))
 			return TEE_ERROR_BAD_PARAMETERS;
 		return ta_store_install(params[0].memref.buffer, params[1].value.a);
 	case TEEP_AGENT_TA_DELETE: /* Delete TA */
 		if ((TEE_PARAM_TYPE_GET(param_types, 0) != TEE_PARAM_TYPE_MEMREF_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_NONE) ||
-				(TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_NONE))
+		    (TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_NONE) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_NONE))
 			return TEE_ERROR_BAD_PARAMETERS;
 		return ta_store_delete(params[0].memref.buffer, params[1].value.a);
 	case TEEP_AGENT_TA_UNWRAP_TA_IMAGE:
 		if ((TEE_PARAM_TYPE_GET(param_types, 0) != TEE_PARAM_TYPE_MEMREF_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_MEMREF_OUTPUT) ||
-				(TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_VALUE_INOUT))
+		    (TEE_PARAM_TYPE_GET(param_types, 1) != TEE_PARAM_TYPE_VALUE_INPUT) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 2) != TEE_PARAM_TYPE_MEMREF_OUTPUT) ||
+		    (TEE_PARAM_TYPE_GET(param_types, 3) != TEE_PARAM_TYPE_VALUE_INOUT))
 			return TEE_ERROR_BAD_PARAMETERS;
-		return teep_message_unwrap_ta_image(params[0].memref.buffer, params[1].value.a, params[2].memref.buffer, &params[3].value.a);
+		return teep_message_unwrap_ta_image(params[0].memref.buffer,
+						    params[1].value.a,
+						    params[2].memref.buffer,
+						    &params[3].value.a);
 
 	default:
 		return TEE_ERROR_NOT_IMPLEMENTED;
