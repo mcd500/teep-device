d384c71f46120665843cd02c479b47a0afbe7e23
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-02-19 10:36:25 +0100

    It was not asking delete request

--- teep-broker-app/dta.sh
+++ teep-broker-app/dta.sh
@@ -3,4 +3,4 @@
 IP=127.0.0.1
 PORT=3000
 
-teep-broker-app --tamurl http://${IP}:${PORT}/api/tam --talist 8d82573a-926d-4754-9353-32dc29997f74
+teep-broker-app --tamurl http://${IP}:${PORT}/api/tam_delete --talist 8d82573a-926d-4754-9353-32dc29997f74
