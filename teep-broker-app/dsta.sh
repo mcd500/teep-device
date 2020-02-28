43575fd6111c30b5773bb1141cb0f87b0b7e0b05
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-02-28 17:11:33 +0900

    Helper scripts for using otrp

--- teep-broker-app/dsta.sh
+++ teep-broker-app/dsta.sh
@@ -3,4 +3,8 @@
 IP=127.0.0.1
 PORT=3000
 
+if [ ! -z "$1" ]; then
+  PORT=$1
+fi
+
 teep-broker-app --tamurl http://${IP}:${PORT}/api/tam_jose_delete --jose --talist 8d82573a-926d-4754-9353-32dc29997f74
--- teep-broker-app/ista.sh
+++ teep-broker-app/ista.sh
@@ -3,4 +3,8 @@
 IP=127.0.0.1
 PORT=3000
 
+if [ ! -z "$1" ]; then
+  PORT=$1
+fi
+
 teep-broker-app --tamurl http://${IP}:${PORT}/api/tam_jose --jose
