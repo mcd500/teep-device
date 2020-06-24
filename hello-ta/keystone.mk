565f136023928601cf6b79855e0f6d3e3990a4d2
kikuchi <kikuchi@lepidum.co.jp>
2020-06-24 19:47:52 +0900

    wip

--- hello-ta/keystone.mk
+++ hello-ta/keystone.mk
@@ -11,7 +11,44 @@ TEE_REF_TA_DIR = $(CURDIR)/../..
 
 CFLAGS += -I. -I$(TEE_REF_TA_DIR)/include
 
-all: hello-ta.keystone.o
+all: hello-ta.keystone
 
 hello-ta.keystone.o: hello-ta.c
 	$(CC) $(CFLAGS) -c $< -o $@
+
+hello-ta.keystone: hello-ta.keystone.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/secure_storage.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/symmetric_key_gcm.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/time.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/asymmetric_key.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/symmetric_key.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/message_digest.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/tee_wrapper.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/random.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/printf.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/tools.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/strcmp.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/startup.o \
+  $(TEE_REF_TA_DIR)/platform/keystone/tee-internal-api-keystone.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/sign.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/keypair.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/seed.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/sha512.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/add_scalar.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/ge.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/fe.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/key_exchange.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/sc.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/ed25519/verify.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/cryptlib/cipher.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/cryptlib/platform_util.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/cryptlib/aes.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/cryptlib/gcm.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/cryptlib/cipher_wrap.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/tiny-AES-c/aes.o \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../libbuild/tiny_sha3/sha3.o \
+  $(TEE_REF_TA_DIR)/build-keystone/sdk/lib/libkeystone-eapp.a \
+  $(TEE_REF_TA_DIR)/build-keystone/sdk/lib/libkeystone-edge.a \
+  $(TEE_REF_TA_DIR)/ref-ta/keystone/Enclave/../../profiler/libprofiler.a
+	$(LINK) -o $@ -static $^
+
