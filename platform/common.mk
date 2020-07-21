f7cd69cb2643ae24d214bb0acb81ad8e63e6ae76
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2020-07-21 02:33:21 +0000

    Pass path required include dir

--- platform/common.mk
+++ platform/common.mk
@@ -70,7 +70,7 @@ hello-ta: libteep
 	$(MAKE) -C $(SOURCE)/hello-ta -f $(PLAT).mk out-dir=$(BUILD)/hello-ta
 
 hello-app: libteep
-	$(MAKE) -C $(SOURCE)/hello-app -f $(PLAT).mk out-dir=$(BUILD)/hello-app
+	$(MAKE) -C $(SOURCE)/hello-app -f $(PLAT).mk out-dir=$(BUILD)/hello-app APP_CFLAGS="$(APP_CFLAGS)"
 
 teep-agent-ta: libteep
 	$(MAKE) -C $(SOURCE)/teep-agent-ta -f $(PLAT).mk out-dir=$(BUILD)/teep-agent-ta
