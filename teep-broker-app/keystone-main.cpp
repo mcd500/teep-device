fac5d9e7112d81be648c4f741cb4b8b48f0a539c
kikuchi <kikuchi@lepidum.co.jp>
2020-07-28 19:03:48 +0900

    add TEEC function stub

--- teep-broker-app/keystone-main.cpp
+++ teep-broker-app/keystone-main.cpp
@@ -15,6 +15,8 @@
 #include "profiler/profiler.h"
 #endif
 
+#include "tee_client_api.h"
+
 /* We hardcode these for demo purposes. */
 const char* enc_path = "teep-agent-ta";
 const char* runtime_path = "eyrie-rt";
@@ -66,3 +68,37 @@ int ocall_invoke_command_callback(invoke_command_t cb_cmd) {}
 #endif
 
 EDGE_EXTERNC_END
+
+TEEC_Result TEEC_InitializeContext(const char *name, TEEC_Context *context)
+{
+
+}
+
+void TEEC_FinalizeContext(TEEC_Context *context)
+{
+
+}
+
+TEEC_Result TEEC_OpenSession(TEEC_Context *context,
+			     TEEC_Session *session,
+			     const TEEC_UUID *destination,
+			     uint32_t connectionMethod,
+			     const void *connectionData,
+			     TEEC_Operation *operation,
+			     uint32_t *returnOrigin)
+{
+
+}
+
+void TEEC_CloseSession(TEEC_Session *session)
+{
+
+}
+
+TEEC_Result TEEC_InvokeCommand(TEEC_Session *session,
+			       uint32_t commandID,
+			       TEEC_Operation *operation,
+			       uint32_t *returnOrigin)
+{
+
+}
