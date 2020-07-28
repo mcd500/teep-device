60b704d1c341fa102c94f1c33af7159e4ba2aa8b
kikuchi <kikuchi@lepidum.co.jp>
2020-07-28 18:47:13 +0900

    fix app link

--- teep-broker-app/keystone-main.cpp
+++ teep-broker-app/keystone-main.cpp
@@ -16,12 +16,11 @@
 #endif
 
 /* We hardcode these for demo purposes. */
-const char* enc_path = "Enclave.eapp_riscv";
+const char* enc_path = "teep-agent-ta";
 const char* runtime_path = "eyrie-rt";
 
 int main(int argc, char** argv)
 {
-#error "FIXME: stub"
     Keystone enclave;
     Params params;
     params.setFreeMemSize(1024*1024);
@@ -41,3 +40,29 @@ int main(int argc, char** argv)
     enclave.run();
     return 0;
 }
+
+EDGE_EXTERNC_BEGIN
+
+unsigned int ocall_print_string(const char* str){
+  printf("%s",str);
+  return strlen(str);
+}
+
+int ocall_open_file(const char* fname, int flags, int perm) {}
+int ocall_close_file(int fdesc) {}
+int ocall_write_file(int fdesc, const char *buf,  unsigned int len) {}
+int ocall_invoke_command_callback_write(const char* str, const char *buf,  unsigned int len) {}
+#if !defined(EDGE_OUT_WITH_STRUCTURE)
+int ocall_read_file(int fdesc, char *buf, size_t len) {}
+int ocall_ree_time(struct ree_time_t *timep) {}
+ssize_t ocall_getrandom(char *buf, size_t len, unsigned int flags){}
+#else
+ob256_t ocall_read_file256(int fdesc) {}
+ree_time_t ocall_ree_time(void) {}
+ob16_t ocall_getrandom16(unsigned int flags) {}
+ob196_t ocall_getrandom196(unsigned int flags) {}
+invoke_command_t ocall_invoke_command_polling(void) {}
+int ocall_invoke_command_callback(invoke_command_t cb_cmd) {}
+#endif
+
+EDGE_EXTERNC_END
