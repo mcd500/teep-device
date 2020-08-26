8e1f2f31de6298c3e351ffc614e262756d7b90a0
kikuchi <kikuchi@lepidum.co.jp>
2020-08-26 19:09:36 +0900

    rename

--- hello-app/optee.mk
+++ hello-app/optee.mk
@@ -3,7 +3,7 @@ out-dir ?= .
 
 all: $(out-dir)/hello-app
 
-$(out-dir)/hello-app: main.c
+$(out-dir)/hello-app: optee-main.c
 	mkdir -p $(out-dir)
 	$(CROSS_COMPILE)gcc  $(APP_CFLAGS) $(APP_LDFLAGS) $^ -lteec -o $@ $(APP_LIBS)
 
