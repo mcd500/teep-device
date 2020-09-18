5e95b77eb6dbd58c9e34dccd3802d58c9d9b7062
kikuchi <kikuchi@lepidum.co.jp>
2020-09-18 19:19:40 +0900

    clean up TEEC_InvokeCommand impl

--- teep-broker-app/keystone-main.cpp
+++ teep-broker-app/keystone-main.cpp
@@ -31,93 +31,122 @@
 const char* enc_path = "teep-agent-ta";
 const char* runtime_path = "eyrie-rt";
 
-std::mutex mutex;
-std::condition_variable cv;
-bool invoking = false;
-invoke_command_t command;
-TEEC_Operation *operation;
-unsigned int command_result;
-
-invoke_command_t pull_invoke_command()
+struct Command
 {
-    std::unique_lock<std::mutex> lock(mutex);
-    cv.wait(lock, []{ return invoking; });
-    return command;
-}
+    invoke_command_t command;
+    TEEC_Operation *operation;
+    unsigned int command_result;
+};
 
-param_buffer_t read_invoke_param(int index, unsigned int offset)
+class CommandQueue
 {
-    //printf("%s: %u\n", __func__, offset);
-    param_buffer_t ret;
-    // TODO: check paramType
-    TEEC_TempMemoryReference *ref = &operation->params[index].tmpref;
-    if (offset > ref->size) {
-        ret.size = 0;
-    } else {
-        unsigned int size = std::min(ref->size - offset, sizeof ret.buf);
-        ret.size = size;
-        memcpy(ret.buf, (const char *)ref->buffer + offset, size);
+private:
+    Command current;
+    std::mutex mutex;
+    std::condition_variable cv;
+    bool invoking = false;
+public:
+
+    invoke_command_t pull_invoke_command()
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        cv.wait(lock, [this]{ return invoking; });
+        return current.command;
     }
-    //printf("%s: %u %u\n", __func__, offset, ret.size);
-    return ret;
-}
 
-void write_invoke_param(int index, unsigned int offset, unsigned int size, const char *buf)
-{
-    //printf("%s: %u\n", __func__, offset);
-    // TODO: check paramType
-    TEEC_TempMemoryReference *ref = &operation->params[index].tmpref;
-    if (offset > ref->size) {
-    } else {
-        unsigned int n = std::min<size_t>(ref->size - offset, size);
-        memcpy((char *)ref->buffer + offset, buf, n);
+    param_buffer_t read_invoke_param(int index, unsigned int offset)
+    {
+        param_buffer_t ret;
+        // TODO: check paramType
+        TEEC_TempMemoryReference *ref = &current.operation->params[index].tmpref;
+        if (offset > ref->size) {
+            ret.size = 0;
+        } else {
+            unsigned int size = std::min(ref->size - offset, sizeof ret.buf);
+            ret.size = size;
+            memcpy(ret.buf, (const char *)ref->buffer + offset, size);
+        }
+        return ret;
     }
-}
 
-void put_invoke_command_result(invoke_command_t cmd, unsigned int result)
-{
-    std::unique_lock<std::mutex> lock(mutex);
-    for (int i = 0; i < 4; i++) {
-        switch (TEEC_PARAM_TYPE_GET(cmd.paramTypes, i)) {
-        default:
-            break;
-        case TEEC_VALUE_OUTPUT:
-        case TEEC_VALUE_INOUT:
-            operation->params[i].value.a = cmd.params[i].a;
-            operation->params[i].value.b = cmd.params[i].b;
-            break;
+    void write_invoke_param(int index, unsigned int offset, unsigned int size, const char *buf)
+    {
+        // TODO: check paramType
+        TEEC_TempMemoryReference *ref = &current.operation->params[index].tmpref;
+        if (offset > ref->size) {
+        } else {
+            unsigned int n = std::min<size_t>(ref->size - offset, size);
+            memcpy((char *)ref->buffer + offset, buf, n);
         }
     }
-    command_result = result;
-    operation = NULL;
-    invoking = false;
-    lock.unlock();
-    cv.notify_one();
-}
 
-void put_invoke_command(const invoke_command_t& c, TEEC_Operation *op)
-{
-    std::unique_lock<std::mutex> lock(mutex);
-    command = c;
-    operation = op;
-    invoking = true;
-    lock.unlock();
-    cv.notify_one();
-}
+    void put_invoke_command_result(const invoke_command_t& cmd, unsigned int result)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        for (int i = 0; i < 4; i++) {
+            switch (TEEC_PARAM_TYPE_GET(cmd.paramTypes, i)) {
+            default:
+                break;
+            case TEEC_VALUE_OUTPUT:
+            case TEEC_VALUE_INOUT:
+                current.operation->params[i].value.a = cmd.params[i].a;
+                current.operation->params[i].value.b = cmd.params[i].b;
+                break;
+            case TEEC_MEMREF_TEMP_OUTPUT:
+            case TEEC_MEMREF_TEMP_INOUT:
+                current.operation->params[i].tmpref.size = cmd.params[i].size;
+                break;
+            }
+        }
+        current.command_result = result;
+        current.operation = NULL;
+        invoking = false;
+        lock.unlock();
+        cv.notify_one();
+    }
 
-int pull_invoke_command_result()
-{
-    std::unique_lock<std::mutex> lock(mutex);
-    cv.wait(lock, []{ return !invoking; });
-    return command_result;
-}
+    void put_invoke_command(int commandID, TEEC_Operation *operation)
+    {
+        invoke_command_t c;
+        c.commandID = commandID;
+        c.paramTypes = operation ? operation->paramTypes : 0;
+        for (int i = 0; i < 4; i++) {
+            c.params[i].a = 0;
+            c.params[i].b = 0;
+            c.params[i].size = 0;
+            switch (TEEC_PARAM_TYPE_GET(c.paramTypes, i)) {
+            default:
+                break;
+            case TEEC_VALUE_INPUT:
+            case TEEC_VALUE_INOUT:
+                c.params[i].a = operation->params[i].value.a;
+                c.params[i].b = operation->params[i].value.b;
+                break;
+            case TEEC_MEMREF_TEMP_INPUT:
+            case TEEC_MEMREF_TEMP_OUTPUT:
+            case TEEC_MEMREF_TEMP_INOUT:
+                c.params[i].size = operation->params[i].tmpref.size;
+                break;
+            }
+        }
+        std::unique_lock<std::mutex> lock(mutex);
+        current.command = c;
+        current.operation = operation;
+        invoking = true;
+        lock.unlock();
+        cv.notify_one();
+    }
 
-int my_TEEC_InvokeCommand(const invoke_command_t& c)
-{
-    put_invoke_command(c, NULL);
-    return pull_invoke_command_result();
-}
+    int pull_invoke_command_result()
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        cv.wait(lock, [this]{ return !invoking; });
+        return current.command_result;
+    }
 
+};
+
+static CommandQueue queue;
 
 TEEC_Result TEEC_InvokeCommand(TEEC_Session *session,
 			       uint32_t commandID,
@@ -126,30 +155,8 @@ TEEC_Result TEEC_InvokeCommand(TEEC_Session *session,
 {
     fprintf(stderr, "TEEC_InvokeCommand\n");
 
-    invoke_command_t c;
-    c.commandID = commandID;
-    c.paramTypes = operation->paramTypes;
-    for (int i = 0; i < 4; i++) {
-        c.params[i].a = 0;
-        c.params[i].b = 0;
-        c.params[i].size = 0;
-        switch (TEEC_PARAM_TYPE_GET(c.paramTypes, i)) {
-        default:
-            break;
-        case TEEC_VALUE_INPUT:
-        case TEEC_VALUE_INOUT:
-            c.params[i].a = operation->params[i].value.a;
-            c.params[i].b = operation->params[i].value.b;
-            break;
-        case TEEC_MEMREF_TEMP_INPUT:
-        case TEEC_MEMREF_TEMP_OUTPUT:
-        case TEEC_MEMREF_TEMP_INOUT:
-            c.params[i].size = operation->params[i].tmpref.size;
-            break;
-        }
-    }
-    put_invoke_command(c, operation);
-    unsigned int ret = pull_invoke_command_result();
+    queue.put_invoke_command(commandID, operation);
+    unsigned int ret = queue.pull_invoke_command_result();
 
     return ret;
 }
@@ -179,12 +186,8 @@ int main(int argc, const char** argv)
     );
 
     int ret = broker_main();
-    {
-        invoke_command_t c;
-        c.commandID = TEEP_AGENT_TA_EXIT;
-        c.paramTypes = 0;
-        my_TEEC_InvokeCommand(c);
-    }
+
+    TEEC_InvokeCommand(NULL, TEEP_AGENT_TA_EXIT, NULL, NULL);
     enclave_thread.join();
 
     return ret;
@@ -194,23 +197,23 @@ EDGE_EXTERNC_BEGIN
 
 invoke_command_t ocall_pull_invoke_command()
 {
-    return pull_invoke_command();
+    return queue.pull_invoke_command();
 }
 
 param_buffer_t ocall_read_invoke_param(int index, unsigned int offset)
 {
-    return read_invoke_param(index, offset);
+    return queue.read_invoke_param(index, offset);
 }
 
 void ocall_write_invoke_param(int index, unsigned int offset, unsigned int size, const char *buf)
 {
     //lwsl_hexdump_notice(buf, size);
-    write_invoke_param(index, offset, size, buf);
+    queue.write_invoke_param(index, offset, size, buf);
 }
 
 void ocall_put_invoke_command_result(invoke_command_t cmd, unsigned int result)
 {
-    return put_invoke_command_result(cmd, result);
+    return queue.put_invoke_command_result(cmd, result);
 }
 
 EDGE_EXTERNC_END
