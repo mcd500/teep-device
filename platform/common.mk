a8936858abed557fe10cf24ef334f507bca6f5ba
kikuchi <kikuchi@lepidum.co.jp>
2020-10-02 18:19:20 +0900

    add -Wno-enum-conversion to libwebsockets build

--- platform/common.mk
+++ platform/common.mk
@@ -3,7 +3,6 @@
 libteep: libteep-host libteep-ree libteep-tee
 hello: hello-ta hello-app
 teep: teep-agent-ta teep-broker-app
-export CFLAGS=-Wno-enum-conversion
 
 libteep-host: libteep-mbedtls-host libteep-libwebsockets-host
 libteep-ree: libteep-mbedtls-ree libteep-libwebsockets-ree libteep-libteep-ree
