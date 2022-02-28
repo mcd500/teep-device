302ebdc2a98cecb77b436a005af6ee534bbd9460
qsfix-nagaraja <nagarajat@cabcsgroup-india.com>
2022-02-28 15:05:39 +0900

    Changed the tee-internal-api.h to other files

--- teep-agent-ta/teep-agent-ta.c
+++ teep-agent-ta/teep-agent-ta.c
@@ -32,7 +32,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include <tee_internal_api.h>
+#include "tee-common.h"
 //#include <libwebsockets.h>
 #include <libteep.h>
 #include <teesuit.h>
