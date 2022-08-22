618bd7f247b9d2fb8a8ed69c9a0e08c055f71ac1
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2022-08-22 17:33:30 +0900

    Just comments

--- key/genkey.sh
+++ key/genkey.sh
@@ -2,40 +2,42 @@
 
 set -ex
 
-# P-256 = secp256r1 = prime256v1
-# P-384 = secp384r1 = ansip384r1
-# P-521 = secp521r1 = ansip521r1
+# ES256 = P-256 = secp256r1 = prime256v1
+# ES384 = P-384 = secp384r1 = ansip384r1
+# ES385 = P-521 = secp521r1 = ansip521r1
 
 EC=P256
 SECP=secp256r1
 
-# generate teep keys
+# Generate teep key pairs
 
 for p in tc-signer tam-teep device-teep; do
     openssl ecparam -name $SECP -genkey -noout -out $p-$EC-priv.pem
     openssl ec -in $p-$EC-priv.pem -pubout -out $p-$EC-pub.pem
 done
 
-# PKI stuffs
-
+# Generate key pairs for CAs
 for p in rootca tc-signer-ca tam-teep-ca device-teep-ca; do
     mkdir -p CAs/$p
     openssl ecparam -name $SECP -genkey -noout -out CAs/$p/$EC-priv.pem
 done
 
-C="/C=JP/ST=Kantou/L=Tokyo/O=AIST/OU=AIST teep"
+# Information of Certificate Attributes
+C="/C=JP/ST=Tokyo/L=Koto-ku/O=AIST/OU=CPSEC"
 
+# Generating rootca certs
 for p in rootca; do
     openssl req -x509 -new -days +3650 -sha512 \
         -key CAs/$p/$EC-priv.pem \
-        -subj "$C/CN=teep sample rootca" \
+        -subj "$C/CN=$p" \
         -out CAs/$p/$EC-cert.pem
 done
 
+# Generating Intermediate CA certs signed by the root private key
 for p in tc-signer-ca tam-teep-ca device-teep-ca; do
     openssl req -new -sha512 \
         -key CAs/$p/$EC-priv.pem \
-        -subj "$C/CN=teep sample $p" \
+        -subj "$C/CN=$p" \
         -out CAs/$p/$EC-cert.csr
     openssl x509 -req -days +3650 -sha512 \
         -in CAs/$p/$EC-cert.csr \
@@ -46,12 +48,11 @@ for p in tc-signer-ca tam-teep-ca device-teep-ca; do
         -out CAs/$p/$EC-cert.pem
 done
 
-# sign teep keys
-
+# Creating signed certs by private keys of Intermediate CAs
 for p in tc-signer tam-teep device-teep; do
     openssl req -new -sha512 \
         -key $p-$EC-priv.pem \
-        -subj "$C/CN=teep sample $p" \
+        -subj "$C/CN=$p" \
         -out $p-$EC.csr
     openssl x509 -req -days +3650 -sha512 \
         -in $p-$EC.csr \
@@ -59,4 +60,5 @@ for p in tc-signer tam-teep device-teep; do
         -CAkey CAs/$p-ca/$EC-priv.pem \
         -CAcreateserial \
         -out $p-$EC-cert.pem
+    rm -f $p-$EC.csr
 done
